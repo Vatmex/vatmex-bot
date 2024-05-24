78897073b2be680aee5187a9774dd6f476aeadf5
Gustavo Valdez <gvaldezsan@gmail.com>
2024-05-24 16:13:15 -0600

    Update ids

--- src/models/callsigns.js
+++ src/models/callsigns.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 const updateRigoberto = async (bot, callback) => {
   const server = bot.guilds.cache.get(process.env.GUILD_ID);
-  const rigoberto = await server.members.fetch('611929847601823760');
+  const rigoberto = await server.members.fetch('776324184874680331');
 
   if (rigoberto.nickname !== 'Rigo el Puma - 1523823') {
     bot.channels.cache
-      .get('693320172760596523')
+      .get('1225472784218718238')
       .send(`<@${rigoberto.id}> no seas necio!`);
     rigoberto.setNickname('Rigo el Puma - 1523823');
   }
